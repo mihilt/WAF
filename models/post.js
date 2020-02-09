fd55c7aaf6468b0e035fb0e070e55b7139348c0c
mihilt <50368758+mihilt@users.noreply.github.com>
2020-02-09 12:10:01 +0900

    c

--- models/post.js
+++ models/post.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose');
 
 // schema
 var postSchema = mongoose.Schema({
-  title:{type:String, required:[true,'Title is required!']}, 
+  title:{type:String, required:[true,'Title is required!']},
   body:{type:String, required:[true,'Body is required!']},
+  author:{type:mongoose.Schema.Types.ObjectId, ref:'user', required:true}, // 1
   createdAt:{type:Date, default:Date.now},
   updatedAt:{type:Date},
 });
